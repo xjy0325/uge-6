482ca34094e92d5592609ef407ce00b6aca60560
xjy0325 <marieyu0325@gmail.com>
2025-10-16 21:07:45 +0200

    change

--- src/main.c
+++ src/main.c
@@ -1,3 +1,11 @@
+// Title: Knight’s tour
+
+// Author: Xingjia Yu
+
+// Description: The program begins on a square
+// and makes greedy move
+// each square returns a number of total moves
+
 #include <stddef.h>
 
 void greedy_tour_from_each_square();
